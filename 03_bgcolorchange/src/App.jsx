f013e3daf642efdf0590d3d04e00ade805b53f0a
Alfiya Shekh <alfiyashekh521@gmail.com>
2026-03-28 21:09:24 +0530

    Change bg color

--- 03_bgcolorchange/src/App.jsx
+++ 03_bgcolorchange/src/App.jsx
@@ -4,13 +4,22 @@ import './App.css'
 import Card from './components/Card'
 function App() {
 
+   const [color,setColor]=useState('pink');
+  // function changeColor(color){
+  //   setColor(color)
+  // }
 
   return (
     <>
-      <h1 className='text-3xl bg-green-500 p-2'>Vite with Tailwind</h1>
-      <Card username="alfiya" post="Manager" url="https://images.pexels.com/photos/7803385/pexels-photo-7803385.jpeg"/>
-      <Card post="CEO"/>
-      <Card />
+     <div className='w-full h-screen duration-0 ' style={{backgroundColor:color}}>
+         <div className=' fixed flex flex-wrap justify-center bottom-1.5 inset-x-0 px-2'>
+          <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+              <button onClick={()=>setColor('red')} className='p-3 rounded-2xl bg-red-500'>Red</button>{" "}
+              <button onClick={()=>setColor('green')} className='p-3 rounded-2xl bg-green-400'>Green</button>{" "}
+              <button onClick={()=>setColor('blue')} className='p-3 rounded-2xl bg-blue-700'>blue</button>{" "}
+          </div>
+         </div>
+     </div>
     </>
   )
 }
